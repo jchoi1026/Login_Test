10bb0f9e83c9e4e76a66d9f81e3bd09f8d96ccee
Jabin Choi <jabinchoi1026@gmail.com>
2024-02-18 01:09:37 -0500

    Revert "Revert "create base_page and login_page objects""
    This reverts commit 57d36e609db32c3594278b82c2fb9ecc232cf851.

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class BasePage():
@@ -7,3 +8,16 @@ class BasePage():
 
     def _open_url(self, url: str):
         self._driver.get(url)
+
+    def _find(self, locator: tuple) -> WebElement:
+        return self._driver.find_element(*locator)
+
+    def _click(self, locator: tuple):
+        self._find(locator).click()
+
+    def _get_text(self, locator: tuple) -> str:
+        return self._find(locator).text
+
+    @property
+    def current_url(self) -> str:
+        return self._driver.current_url
